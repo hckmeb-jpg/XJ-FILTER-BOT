e075bbe2424dab85bf129b5edac61d8b5cc3e5d3
hckmeb-jpg <hckmeb@gmail.com>
2025-09-28 06:16:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'Abidxbot')
 API_ID = int(environ.get('API_ID', '25743266'))
 API_HASH = environ.get('API_HASH', '316af9392f920cc4d5d638bdf3e7d6ee')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8304747226:AAH_kygjS6V0YCRz4m4SXILFqc-xKbtXu5Q")
+BOT_TOKEN = environ.get('BOT_TOKEN', "-xKbtXu5Q")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -196,5 +196,6 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
